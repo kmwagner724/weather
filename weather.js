751ea3d4d11895e41de911962b20d98acebcbedf
kmwagner724 <kwagner2016@kellogg.northwestern.edu>
2016-01-20 16:48:21 -0600

    udpate
    hw

--- weather.js
+++ weather.js
@@ -9,7 +9,7 @@ $(function() {
     window.data = data;
     
     // Put your code here. Don't change any other code in this file. You will be sad.
-    var markup = "Current weather: " + data.currently.summary + " and " + data.currently.temperature + " degrees F";
+    var markup = "Current weather: " + data.currently.summary + " and " + data.currently.temperature + " degrees F. // " + data.daily.summary;
     // End of your code. No, really. Don't change anything below this, or above line 11.
 
     // Takes the contents of the "markup" variable (which should contain HTML) 
